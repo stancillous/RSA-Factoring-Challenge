77962c7b940dc14a500c345bd958c21bb6c0d410
stancillous <stancillousray@gmail.com>
2023-06-04 03:16:41 -0700

    rsa factors

--- factors.c
+++ factors.c
@@ -1,41 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv)
-{
-  if (argc != 2)
-  {
-    printf("usage error\n");
-    return (1);
-  }
+int main(int argc, char *argv[]) {
+    FILE *file;
+    size_t count;
+    ssize_t line;
+    char *buffer = NULL;
 
-  FILE *file = fopen(argv[1], "r");
-  if (file == NULL)
-  {
-    printf("failed to open file");
-    return (1);
-  }
-  char line[98];
-  while (fgets(line, sizeof(line), file) != NULL)
-    {
-      unsigned long long int num = atoi(line), counter = 2, fact1, fact2;
-
-      while (counter <= num)
-        {
-          if (num % counter == 0)
-          {
-            fact1 = counter;
-            fact2 = num / counter;
-            break;
-          }
-            counter++;
-        }
-
-      printf("%llu = %llu * %llu\n", num, fact2, fact1);
-
-      
+    if (argc != 2) {
+        fprintf(stderr, "Usage Error\n");
+        return 1;
     }
 
-  return (0);
+    file = fopen(argv[1], "r");
+    if (file == NULL) {
+        fprintf(stderr, "Error: can't open file %s\n", argv[1]);
+        return 1;
+    }
 
+    while ((line = getline(&buffer, &count, file)) != -1) {
+        unsigned long long int num, i;
+
+        num = strtoull(buffer, NULL, 10);
+
+        for (i = 2; i < num; i++) {
+            if (num % i == 0) {
+                printf("%llu=%llu*%llu\n", num, num / i, i);
+                break;
+            }
+        }
+    }
+
+    free(buffer);
+    fclose(file);
+
+    return 0;
 }
+
